14b11a69f7982d5036d4daa47408990c9851ec4b
Andrew Blakey <ablakey@gmail.com>
2021-10-17 14:25:09 -0400

    progres on board class

--- src/board.ts
+++ src/board.ts
@@ -1,23 +1,54 @@
 export class Board {
-  public size: number;
-  public cells: HTMLDivElement[] = [];
+  private size: number;
+  private cells: HTMLDivElement[] = [];
+  private cellState: number[];
+  private colorMap: Map<number, string | null>;
 
-  constructor(size: number) {
+  constructor(size: number, colorMap: Map<number, string | null>) {
     this.size = size;
+    this.colorMap = colorMap;
 
+    // TODO: generate the board in JS so we don't need to provide any CSS.
+    // TODO: require a targetElementId, where we place the square element.
+
+    // Populate square with a grid div elements as cells.
+    const square = document.querySelector(".square")!;
     for (let cell = 0; cell < size * size; cell++) {
-      const element = document.createElement("div");
-      document.querySelector(".square").appendChild(element);
-      // element.classList.toggle("none");
-      this.cells.push(element);
+      const cell = document.createElement("div");
+      square.appendChild(cell);
+      this.cells.push(cell);
     }
 
-    setInterval(() => {
-      this.cells.forEach((c) => {
-        if (Math.random() > 0.95) {
-          c.classList.toggle("none");
+    // Populate cellState as a simple row-major array. This allows us to avoid
+    // Doing DOM lookups for state. We just need to be careful to ensure this state
+    // remains inline with DOM state.
+    this.cellState = new Array(size ** 2).fill(0);
+  }
+
+  public update(newCellState: number[][]) {
+    if (newCellState.length !== this.size ** 2) {
+      throw new Error("newCellState must be the same size as Board.");
+    }
+
+    newCellState
+      .flat()
+      .map((n, idx) => (n !== this.cellState[idx] ? idx : undefined))
+      .forEach((idx) => {
+        if (idx === undefined) {
+          return;
+        }
+
+        const newColor = this.colorMap.get(newCellState[idx]);
+        if (newColor === undefined) {
+          throw new Error("Tried to set state to a color not provided in colorMap.");
+        } else if (newColor === null) {
+          this.cells[idx].style.opacity = "0";
+        } else {
+          this.cells[idx].style.opacity = "1";
+          this.cells[idx].style.background = newColor;
         }
       });
-    }, 100);
+
+    this.cellState = newCellState;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,30 @@
 import { Board } from "./board";
 
-window.onload = () => {
-  new Board(20);
-};
+enum State {
+  Empty,
+  Snake,
+  Food,
+}
+
+const colorMap = new Map([
+  [State.Empty, null], // null hides the cell.
+  [State.Snake, "#000"],
+  [State.Food, "#C00"],
+]);
+
+function main() {
+  const board = new Board(20, colorMap);
+
+  setInterval(() => {
+    const newState = [];
+
+    for (let x = 0; x < 400; x++) {
+      const n = Math.floor(Math.random() * 3);
+      newState.push(n);
+    }
+
+    board.update(newState);
+  }, 1000);
+}
+
+window.onload = main;
