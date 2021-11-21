4976b1c1889b401d9a3bb928a0144cf47de613cd
Andrew Blakey <ablakey@gmail.com>
2021-11-20 21:28:01 -0500

    proress helpers

--- src/Utilities/QueryHelpers.ts
+++ src/Utilities/QueryHelpers.ts
@@ -2,6 +2,7 @@ import { BaseUtility } from "./BaseUtility";
 
 export class QueryHelpers extends BaseUtility {
   getNextTarget(prevTarget: string): string | undefined {
+    this.ecs.query(["AI"]);
     return undefined;
   }
 }
